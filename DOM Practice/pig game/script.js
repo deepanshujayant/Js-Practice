515752a985aaa605ae56e9baa54927e34fd2d2e9
deepu97 <deepanshujayant@gmail.com>
2020-10-31 01:48:00 +0530

    DOM -v

--- DOM Practice/pig game/script.js
+++ DOM Practice/pig game/script.js
@@ -1,18 +1,30 @@
 'use strict';
-var scores, totalscore, activeP;
-scores = [0,0];
-totalscore = 0;
-activeP = 0;
+var scores, totalscore, activeP, gamePlaying;
 
+
+function init(){
+    scores = [0,0]; 
+    totalscore = 0;
+    activeP = 0;
+    gamePlaying = true;
+    document.getElementById('score--0').textContent = '0';
+    document.getElementById('score--1').textContent = '0';
+    document.getElementById('current--0').textContent = '0';
+    document.getElementById('current--1').textContent = '0';
+    document.getElementById('name--0').textContent = 'Player 1';
+    document.getElementById('name--1').textContent = 'Player 2';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player--0').classList.remove('player--winner');
+    document.querySelector('.player--1').classList.remove('player--winner');
+    document.querySelector('.player--0').classList.remove('player--active');
+    document.querySelector('.player--1').classList.remove('player--active');
+    document.querySelector('.player--0').classList.add('player--active');
+
+}
 //console.log(dice);
-
+init();
 //document.querySelector('#current--'+ activeP).textContent = dice;
-document.getElementById('score--0').textContent = '0';
-document.getElementById('score--1').textContent = '0';
-document.getElementById('current--0').textContent = '0';
-document.getElementById('current--1').textContent = '0';
 
-document.querySelector('.dice').style.display = 'none';
 
 function next(){
     activeP  === 0 ? activeP = 1 : activeP = 0;
@@ -31,27 +43,31 @@ function next(){
 
 document.querySelector('.btn--roll').addEventListener('click', function(){
     //random num
-    var dice = Math.floor(Math.random()*6) + 1;
+    if(gamePlaying){
+        var dice = Math.floor(Math.random()*6) + 1;
     //display dice
-    var diceDOM = document.querySelector('.dice'); 
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-'+ dice + '.png'; 
+        var diceDOM = document.querySelector('.dice'); 
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-'+ dice + '.png'; 
 
-    // update total
-    if(dice !== 1){
-        totalscore += dice;
-        document.querySelector('#current--'+ activeP).textContent = totalscore;
-        document.querySelector('#score--'+ activeP).textContent = totalscore;
-       // document.querySelector('#current--'+ activeP).textContent = totalscore;
+        // update total
+        if(dice !== 1){
+            totalscore += dice;
+            document.querySelector('#current--'+ activeP).textContent = totalscore;
+            //document.querySelector('#score--'+ activeP).textContent = totalscore;
+        // document.querySelector('#current--'+ activeP).textContent = totalscore;
 
-    }else {
-        next();
+        }else {
+            next();
+        }
     }
-})
+    
+});
 
 document.querySelector('.btn--hold').addEventListener('click', function(){
     // add curr to total
-    scores[activeP] += totalscore;
+    if(gamePlaying){
+        scores[activeP] += totalscore;
     
     // update ui
     document.querySelector('#score--'+activeP).textContent = scores[activeP];
@@ -62,11 +78,14 @@ document.querySelector('.btn--hold').addEventListener('click', function(){
         document.querySelector('.dice').style.display = 'none';
         document.querySelector('.player--'+ activeP).classList.add('player--winner');
         document.querySelector('.player--'+ activeP).classList.remove('player--active');
+        gamePlaying = false;
     }
     else{
         next();
     }
+    }
 
     //next();
-})
+});
 
+document.querySelector('.btn--new').addEventListener('click', init);
